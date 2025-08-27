2a5ac9ea2e26c1854ee5851b98cf7479f3e15a3e
Sandro Xuskivadze <xuskivadzesandro08@gmail.com>
2025-08-27 23:08:14 +0400

    FINAL

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // banner animation loop logic
 
+let bannerAnimationDuration = 12900;
+
 let animationTargets = [
     background = document.querySelector('.scene-1-background'),
     burger = document.querySelector('.scene-1-burger'),
     claim = document.querySelector('.scene-1-claim'),
     logo = document.querySelector('.scene-1-logo'),
     headline = document.querySelector('.scene-1-headline'),
+    headline_overlay = document.querySelector('.scene-1-headline_overlay'),
     text_2 = document.querySelector('.scene-1-txt_2'),
     mcextreme_container = document.querySelector('.scene-1-mcextreme_container'),
     x = document.querySelector('.scene-1-x'),
@@ -16,8 +19,7 @@ let animationTargets = [
     circle3 = document.querySelector('.outro-circle3'),
     logo_container = document.querySelector('.scene-2-logo_container'),
     mcextreme = document.querySelector('.scene-2-mcextreme'),
-    overlay = document.querySelector('.scene-2-overlay'),
-    headline_overlay = document.querySelector('.scene-1-headline_overlay')
+    scene2_overlay = document.querySelector('.scene-2-overlay')
 ];
 
 function stopAnimation(target, stopDelay) {
@@ -33,34 +35,12 @@ function startAnimation() {
             target.style.display = 'inline';
         } else {
             target.style.display = 'block';
-        }
+        };
         target.classList.remove('paused');
         target.classList.add('running');
     });
     stopBannerAnimation();
 };
-
-function stopBannerAnimation() {
-    stopAnimation(animationTargets[0], 450);
-    stopAnimation(animationTargets[1], 2150);
-    stopAnimation(animationTargets[2], 1050);
-    stopAnimation(animationTargets[3], 1050);
-    stopAnimation(animationTargets[4], 2160);
-    stopAnimation(animationTargets[5], 2650);
-    stopAnimation(animationTargets[6], 9750);
-    stopAnimation(animationTargets[7], 4770);
-    stopAnimation(animationTargets[8], 5350);
-    stopAnimation(animationTargets[9], 5950);
-    stopAnimation(animationTargets[10], 9850);
-    stopAnimation(animationTargets[11], 9650);
-    stopAnimation(animationTargets[12], 8250);
-    stopAnimation(animationTargets[13], 8750);
-    stopAnimation(animationTargets[14], 9750);
-    stopAnimation(animationTargets[15], 11060);
-    stopAnimation(animationTargets[16], 4660);
-};
-
-stopBannerAnimation();
 
 function startOver() {
     for(let i = 0; i < animationTargets.length; i++) {
@@ -69,10 +49,36 @@ function startOver() {
     };
 };
 
-setInterval(() => {
-    startOver();
-}, 11500);
+function stopBannerAnimation() {
+    stopAnimation(animationTargets[0], 450); // .scene-1-background
+    stopAnimation(animationTargets[1], 2150); // .scene-1-burger
+    stopAnimation(animationTargets[2], 1050); // .scene-1-claim
+    stopAnimation(animationTargets[3], 1050); // .scene-1-logo
+    stopAnimation(animationTargets[4], 2160); // .scene-1-headline
+    stopAnimation(animationTargets[5], 4660); // .scene-1-headline_overlay
+    stopAnimation(animationTargets[6], 2650); // .scene-1-txt_2
+    stopAnimation(animationTargets[7], 9750); // .scene-1-mcextreme_container
+    stopAnimation(animationTargets[8], 4760); // .scene-1-x
+    stopAnimation(animationTargets[9], 5350); // .scene-1-mce_treme
+    stopAnimation(animationTargets[10], 5950); // .scene-1-stroerer
+    stopAnimation(animationTargets[11], 9950); // .outro-circle1
+    stopAnimation(animationTargets[12], 10050); // .outro-circle2
+    stopAnimation(animationTargets[13], 9550); // .outro-circle3
+    stopAnimation(animationTargets[14], 9650); // .scene-2-logo_container
+    stopAnimation(animationTargets[15], 10660); // .scene-2-mcextreme
+    stopAnimation(animationTargets[16], 12660); // .scene-2-overlay
+};
 
-setInterval(() => {
-    startAnimation();
-}, 11700);
+stopBannerAnimation();
+
+function replayBannerAnimation() {
+    setInterval(() => {
+        startOver();
+    }, bannerAnimationDuration);
+
+    setInterval(() => {
+        startAnimation();
+    }, bannerAnimationDuration + 150);
+};
+
+replayBannerAnimation();
